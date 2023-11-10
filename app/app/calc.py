e0d53969e5eb02f4b904cdf7153933ea9cf7c91b
Jose Rios <jrbernabe@gmail.com>
2023-11-10 15:36:16 -0400

    Add example test

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(a, b):
+    """Add a and b and return result"""
+    return a + b
+
+
+def subtract(a, b):
+    """Subtract b from a and return result"""
+    return a - b
