3a372d8ffec756db151f0b4c24a1d8ee48dfd514
Jose Rios <jrbernabe@gmail.com>
2023-11-14 11:08:41 -0400

    Implement custom User model for authentication and migrate models

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,19 @@
+"""Tests for models."""
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = 'test@email.com'
+        password = 'password'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
