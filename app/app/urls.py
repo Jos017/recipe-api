5411f70e43a8bf4918bc67cf051daf650ec1bf33
Jose Rios <jrbernabe@gmail.com>
2023-11-16 16:03:22 -0400

    Add user/create endpoint with POST method support

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -28,4 +28,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
     ),
+    path('api/user/', include('user.urls')),
 ]
