b467ed18dea78281d091dc6c16fce500e6110acf
Jose Rios <jrbernabe@gmail.com>
2023-11-27 13:42:59 -0400

    Implement creating ingredients with recipe feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 
 class IngredientSerializer(serializers.ModelSerializer):
-    """Serialzier for Ingredients."""
+    """Serializer for Ingredients."""
 
     class Meta:
         """Ingredient serializer properties."""
@@ -28,13 +28,22 @@ class TagSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
 
+    ingredients = IngredientSerializer(many=True, required=False)
     tags = TagSerializer(many=True, required=False)
 
     class Meta:
         """Serializer Properties."""
 
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        fields = [
+            'id',
+            'title',
+            'time_minutes',
+            'price',
+            'link',
+            'tags',
+            'ingredients',
+        ]
         read_only_fields = ['id']
 
     def _get_or_create_tags(self, tags, recipe):
@@ -47,11 +56,23 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """Handle getting or creating ingredients as needed."""
+        auth_user = self.context['request'].user
+        for ingredient in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredient,
+            )
+            recipe.ingredients.add(ingredient_obj)
+
     def create(self, validated_data):
         """Create a recipe."""
+        ingredients = validated_data.pop('ingredients', [])
         tags = validated_data.pop('tags', [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
